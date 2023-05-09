617962682d34c49f33d7540e301ab96535a23783
fernandojosecc <fernandojosecc@hotmail.com>
2023-05-08 21:18:06 -0700

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ export class AppComponent {
   name = 'Nicolas';
   age = 20;
   img = 'https://depor.com/resizer/zYAewNYe84T1H3sm_y61ClAywWI=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/K74I6DN7S5HDZKGZMZ7SJX5ARE.jpg';
+  btnDisabled = true;
+  person={
+    name :'Nicolas',
+    age : 20,
+    avatar:'https://depor.com/resizer/zYAewNYe84T1H3sm_y61ClAywWI=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/K74I6DN7S5HDZKGZMZ7SJX5ARE.jpg',
+  }
 }
