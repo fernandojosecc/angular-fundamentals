cba4c93f769b0dcc356207231869b4cd707ad0a9
fernandojosecc <fernandojosecc@hotmail.com>
2023-05-08 19:14:10 -0700

    agregando documentos

---
+++ src/app/recap.ts
@@ -0,0 +1,16 @@
+const username: string='Nicolas';
+const sum = (a:number,b:number)=>{
+  return a+b;
+}
+sum(1,2);
+
+class Person{
+
+  constructor(public age:number, public lastName:string){
+
+
+  }
+}
+
+const nico = new Person(15,'Molina');
+nico.age;
