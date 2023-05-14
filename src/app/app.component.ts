cd9c8c3817d48f1b49c6c0d49f6325d98b387513
fernandojosecc <fernandojosecc@hotmail.com>
2023-05-14 16:09:11 -0700

    dynamic class and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import {Product} from './product.model'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  WidthImg = 10;
+
   name = 'Nicolas';
   age = 20;
   img = 'https://depor.com/resizer/zYAewNYe84T1H3sm_y61ClAywWI=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/K74I6DN7S5HDZKGZMZ7SJX5ARE.jpg';
