c31b813f54de792eeff2454dec901ca222ce5579
fernandojosecc <fernandojosecc@hotmail.com>
2023-06-05 19:58:04 -0700

    Crear un formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,11 @@ export class AppComponent {
   age = 20;
   img = 'https://depor.com/resizer/zYAewNYe84T1H3sm_y61ClAywWI=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/K74I6DN7S5HDZKGZMZ7SJX5ARE.jpg';
   btnDisabled = true;
+  register ={
+    name: '',
+    email:'',
+    password:'',
+  }
   person={
     name :'Nicolas',
     age : 20,
@@ -85,5 +90,8 @@ export class AppComponent {
 
   deleteName(index:number){
     this.names.splice(index,1);
+  }
+  onRegister(){
+    console.log(this.register)
   }
 }
