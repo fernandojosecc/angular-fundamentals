d19da5a92641320dfce4f4806c5abea69a91caec
fernandojosecc <fernandojosecc@hotmail.com>
2023-07-19 21:55:14 -0700

    Componente para producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';
+
+  products: Product[]=[
+    {
+      id:'1',
+      name: 'El mejor juguete',
+      price:565,
+      image: './assets/images/toy.jpg'
+    },
+    {
+      id:'1',
+      name: 'El mejor juguete',
+      price:565,
+      image: './assets/images/toy.jpg'
+    },
+    {
+      id:'1',
+      name: 'El mejor juguete',
+      price:565,
+      image: './assets/images/toy.jpg'
+    }
+  ];
 
   onLoaded(img:string){
     console.log('log padre')
